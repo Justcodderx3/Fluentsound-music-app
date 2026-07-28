a3093d73daad442ff1ac58eaf6e7eb6ed2c80030
Justcodderx3 <e3952959@gmail.com>
2026-07-28 22:32:42 +0300

    feat: add i18n module with en/ru translations dictionary

---
+++ frontend/i18n.py
@@ -0,0 +1,23 @@
+from kivymd.app import MDApp
+
+TRANSLATIONS = {
+    'en': {
+        "username_hint": "Username",
+        "password_hint": "Password",
+        "Login_button": "Login"
+    },
+    'ru': {
+           "username_hint": "Никнейм",
+           "password_hint": "Пароль",
+           "Login_button": "Вход"
+           }
+}
+
+
+
+def _(key: str) -> str:
+    app = MDApp.get_running_app()
+    translate = TRANSLATIONS[app.current_language]
+    if key not in translate:
+        return key
+    return translate[key]
